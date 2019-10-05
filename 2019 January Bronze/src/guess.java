9a5fec454a6dcc31176bc15ed8436cf9d3ed3859
sechoi1300 <52802440+sechoi1300@users.noreply.github.com>
2019-10-05 16:46:38 -0700

    .

--- 2019 January Bronze/src/guess.java
+++ 2019 January Bronze/src/guess.java
@@ -33,7 +33,7 @@ public class guess {
 	}
 	
 	public static void solve() {
-		int same = 1;
+		int same = 0;
 		for(int i = 0; i < N - 1; i++) {
 			for(int j = i + 1; j < N; j++) {
 				for(String p : a[i]) {
