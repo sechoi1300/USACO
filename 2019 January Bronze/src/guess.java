2e6cd5e7750d7513243d8923d40ae82ad59d5546
sechoi1300 <52802440+sechoi1300@users.noreply.github.com>
2019-10-05 16:35:42 -0700

    .

--- 2019 January Bronze/src/guess.java
+++ 2019 January Bronze/src/guess.java
@@ -1,9 +1,51 @@
+import java.util.*;
+import java.io.*;
 
 public class guess {
-
-	public static void main(String[] args) {
+	
+	static Scanner in;
+	static PrintWriter out;
+	static int N, max;
+	static ArrayList<String> [] a;
+	static ArrayList<Integer> last = new ArrayList<Integer>();
+	
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-
+		in = new Scanner(new File("guess.in"));
+		out = new PrintWriter(new File("guess.out"));
+		
+		init();
+		solve();
 	}
-
+	
+	public static void init() {
+		N = in.nextInt();
+		a = new ArrayList[N];
+		max = 0;
+		for(int i = 0; i < N; i++) {
+			a[i] = new ArrayList<String>();
+			in.next();
+			int k = in.nextInt();
+			for(int j = 0; j < k; j++) {
+				a[i].add(in.next());
+			}
+		}
+	}
+	
+	public static void solve() {
+		int same = 1;
+		for(int i = 0; i < N - 1; i++) {
+			for(int j = 0; j < N; j++) {
+				for(String p : a[i]) {
+					if(a[j].contains(p)) {
+						same++;
+					}
+				}
+				max = Math.max(max, same);
+				same = 0;
+			}
+		}
+		out.println(max - 1);
+		out.close();
+	}
 }
