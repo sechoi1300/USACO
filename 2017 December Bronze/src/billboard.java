0f7219580086e84260e220b3effc1377d4923070
sechoi1300 <52802440+sechoi1300@users.noreply.github.com>
2019-10-12 17:19:46 -0700

    .

--- 2017 December Bronze/src/billboard.java
+++ 2017 December Bronze/src/billboard.java
@@ -6,7 +6,7 @@ public class billboard {
 	static Scanner in;
 	static PrintWriter out;
 	static int x1of1, x2of1, x3of1, x4of1, x1of2, x2of2, x3of2, x4of2, x1ofT, x2ofT, x3ofT, x4ofT;
-	static int y1of1, 
+	static int y1of1, y2of1, y3of1, y4of1, y1of2, y2of2, y3of2, y4of2, y1ofT, y2ofT, y3ofT, y4ofT;
 	
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
