020503d408e9f0959f46da4846ac2ec6e656b58b
sechoi1300 <52802440+sechoi1300@users.noreply.github.com>
2019-11-09 16:35:43 -0800

    .

--- 2015 December Bronze/src/badmilk.java
+++ 2015 December Bronze/src/badmilk.java
@@ -1,9 +1,46 @@
+import java.util.*;
+import java.io.*;
 
 public class badmilk {
-
-	public static void main(String[] args) {
+	
+	static Scanner in;
+	static PrintWriter out;
+	static int N, M, D, S;
+	
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-
+		in = new Scanner(new File("badmilk.in"));
+		out = new PrintWriter(new File("badmilk.out"));
+		
+		init();
+		solve();
+		
+		in.close();
+		out.close();
 	}
-
+	
+	public static void init() {
+		N = in.nextInt();
+		M = in.nextInt();
+		D = in.nextInt();
+		S = in.nextInt();
+		for(int i = 0; i < D; i++) {
+			
+		}
+	}
+	
+	public static void solve() {
+		
+	}
+	
 }
+
+/*ArrayList<Integer> [] a = new ArrayList[3];
+
+
+for(int i=0; i<3; i++){
+  a[i] = new ArrayList<Integer>();
+}
+
+a[0].add(0);
+*/w
