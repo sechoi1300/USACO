c4f91f2917448aa06c99ff69e2c437b79ecc9583
sechoi1300 <52802440+sechoi1300@users.noreply.github.com>
2019-11-09 17:27:16 -0800

    .

--- 2016 December Bronze/src/blocks.java
+++ 2016 December Bronze/src/blocks.java
@@ -6,9 +6,8 @@ public class blocks {
 	static Scanner in;
 	static PrintWriter out;
 	static int N;
-	static String[] words;
-	static ArrayList<Character> letters = new ArrayList<Character>();
-	static int[] num = new int[26];
+	static int[] first, second, total;
+	static String f, s;
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -24,24 +23,30 @@ public class blocks {
 	
 	public static void init() {
 		N = in.nextInt();
-		words = new String[N * 2];
-		for(int i = 0; i < N * 2; i ++) {
-			words[i] = in.next();
-		}
-		for(int i = 0; i < N * 2; i++) {
-			char[] temp = words[i].toCharArray();
-			for(int j = 0; j < temp.length; j++) {
-				letters.add(temp[j]);
-			}
-		}
-		Collections.sort(letters);
-		for(int i = 0; i < 26; i++) {
-			
-		}
+        total = new int[26];
 	}
 
 	public static void solve() {
-		out.println(letters);
+		for(int j = 0; j < N; j++) {
+			f = in.next();
+	        s = in.next();
+	        first = new int[26];
+	        second = new int[26];
+			for(int i = 0; i < f.length(); i++) {
+				int temp = f.charAt(i) - 'a';
+				first[temp]++;
+			}
+		    for(int i = 0; i < s.length(); i++) {
+	            int temp = s.charAt(i) - 'a';
+				second[temp]++;
+			}
+		    for(int i = 0; i < 26; i++) {
+	            int max = Math.max(first[i], second[i]); 
+				total[i]+=max;
+			}
+		}
+		for(int i = 0; i < 26; i++) {
+			out.println(total[i]);
+        }
 	}
-	
 }
